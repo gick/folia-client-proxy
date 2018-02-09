9d8f0d1794349b4c151c3ec9208429472f1ef60f
gickhub <gicquel.pierreyves@gmail.com>
2018-02-09 13:41:35 +0100

    folia web

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var express = require('express');
 var proxy = require('express-http-proxy');
 var app=express()
 app.use('/proxy',proxy('localhost:8081'))
-app.use(express.static('../foliaBasecli'));
+app.use(express.static('../folia-web'));
 app.listen(80)
